78957a25daf9cb23d3aa368ea6c875d898901a5b
anandpatel23 <ap947@drexel.edu>
2015-01-30 00:24:21 -0500

    Clean up and user input

--- renaming_files.py
+++ renaming_files.py
@@ -7,16 +7,16 @@ import sys
 def main():
     # Location of Folder
     dataLocation = raw_input("Location of data: ")
+    inputExtension = raw_input("Existing extension: ")
+    outputExtension = raw_input("Requested extension: ")
 
     # Iterates through files and replaces extensions
-
-   # for dirname, dirnames, filenames in os.walk(dataLocation):
     for filename in os.listdir(dataLocation):
             infilename = os.path.join(dataLocation,filename)
             if not os.path.isfile(infilename): continue
             oldbase = os.path.splitext(filename)
             # Change extension here here (.org -> .org.txt, .org -> .txt, etc.)
-            newname = infilename.replace('.org', '.csv')
+            newname = infilename.replace(inputExtension, outputExtension)
             # Rename in OS
             output = os.rename(infilename, newname)
 
